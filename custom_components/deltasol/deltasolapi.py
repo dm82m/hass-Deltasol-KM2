7f52cf6d9810aa266150b12bbf6b8d6de0e88c59
chiefdeputy <6720294+chiefdeputy@users.noreply.github.com>
2022-01-31 21:53:35 +0100

    added support for Resol DL2/DL3 vbus loggers

--- custom_components/deltasol/deltasolapi.py
+++ custom_components/deltasol/deltasolapi.py
@@ -14,41 +14,53 @@ from .const import (
 class DeltasolApi(object):
     """ Wrapper class for Deltasol KM2"""
 
-    def __init__(self, username, password, host):
+    def __init__(self, username, password, host, api_mode="km2"):
         self.data = None
         self.host = host
         self.username = username
         self.password = password
+        self.api_mode = api_mode
 
-    def fetch_data(self):
-        """Use api to get data"""
 
-        url = "http://" + self.host + "/cgi-bin/resol-webservice"
-
-        headers = {
-          'Content-Type': 'application/json'
-        }
-
-        payload = "[{'id': '1','jsonrpc': '2.0','method': 'login','params': {'username': '" + self.username + "','password': '" + self.password + "'}}]"
-        response = requests.request("POST", url, headers=headers, data = payload).json()
-        
-        authId = response[0]['result']['authId']
-        
-        payload = "[{'id': '1','jsonrpc': '2.0','method': 'dataGetCurrentData','params': {'authId': '" + authId + "'}}]"
-        response = requests.request("POST", url, headers=headers, data = payload).json()
-
+    def __parse_data(self, response):
         icon_mapper = defaultdict(lambda: "mdi:flash")
         icon_mapper['°C'] = "mdi:thermometer"
 
         data = {}
-        
+
         j = 0
-        for i in response[0]["result"]["headers"][0]["fields"]:
-            value = response[0]["result"]["headersets"][0]["packets"][0]["field_values"][j]["raw_value"]
+        for i in response["headers"][0]["fields"]:
+            value = response["headersets"][0]["packets"][0]["field_values"][j]["raw_value"]
             if isinstance(value, float):
               value = round(value, 2)
             unit = i["unit"].strip()
             data[i["name"].replace(" ", "_").lower()] = (value, icon_mapper[unit], unit)
             j += 1
-            
+
         return data
+
+
+    def fetch_data(self):
+        """Use api to get data"""
+
+        response = {}
+        if self.api_mode == "km2":
+            url = "http://" + self.host + "/cgi-bin/resol-webservice"
+
+            headers = {
+              'Content-Type': 'application/json'
+            }
+
+            payload = "[{'id': '1','jsonrpc': '2.0','method': 'login','params': {'username': '" + self.username + "','password': '" + self.password + "'}}]"
+            response = requests.request("POST", url, headers=headers, data = payload).json()
+            
+            authId = response[0]['result']['authId']
+            
+            payload = "[{'id': '1','jsonrpc': '2.0','method': 'dataGetCurrentData','params': {'authId': '" + authId + "'}}]"
+            response = requests.request("POST", url, headers=headers, data = payload).json()
+            response = response[0]["result"]
+        elif self.api_mode == "dlx":
+            url = f'http://{self.host}/dlx/download/live?sessionAuthUsername={self.username}&sessionAuthPassword={self.password}'
+            response = requests.request("GET", url).json()
+
+        return self.__parse_data(response)
