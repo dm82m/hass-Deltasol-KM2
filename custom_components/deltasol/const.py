7a5a734d7a5d9c7950509146c5a3e0813b75835b
chris <chris@ostat.com>
2022-02-08 13:10:31 +1000

    changed logger name to include an s 'custom_components.deltasol'.

--- custom_components/deltasol/const.py
+++ custom_components/deltasol/const.py
@@ -1,7 +1,7 @@
 """ Constants for the Resol Deltasol KM2/DL2/DL3 Integration """
 import logging
 
-_LOGGER = logging.getLogger('custom_component.deltasol')
+_LOGGER = logging.getLogger('custom_components.deltasol')
 DOMAIN = "deltasol"
 DEFAULT_NAME = "Resol Deltasol KM2/DL2/DL3"
 DEFAULT_TIMEOUT = 60
