201d2c5273321b4bae3f364ca945ceb8e047b7a7
dm82m <dirk@maucher-online.de>
2022-02-20 09:06:40 +0100

    #12 fine tuned the work, km2 detection by matcher, beautifying the logging, ...

--- custom_components/deltasol/deltasolapi.py
+++ custom_components/deltasol/deltasolapi.py
@@ -21,7 +21,7 @@ class DeltasolApi(object):
         self.username = username
         self.password = password
         self.api_key = api_key
-        self.product= None
+        self.product = None
 
     def __parse_data(self, response):
         icon_mapper = defaultdict(lambda: "mdi:flash")
@@ -50,22 +50,25 @@ class DeltasolApi(object):
 
         try:
             url = f"http://{self.host}/cgi-bin/get_resol_device_information"
-            _LOGGER.debug(f"Detecting resol product {url}")
+            _LOGGER.info(f"Auto detecting Resol Deltasol product from {url}")
             response = requests.request("GET", url)
             if(response.status_code == 200):
-                _LOGGER.debug(f"response {response.text}")
+                _LOGGER.debug(f"response: {response.text}")
                 matches = re.search(r'product\s=\s["](.*?)["]', response.text)
                 if matches:
                     self.product = matches.group(1).lower()
+                    _LOGGER.info(f"Detected Resol Deltasol product: {self.product}")
                 else:
-                     self.product = "km2"
-                _LOGGER.debug(f"Detected {self.product}")
-                return self.product
+                    _LOGGER.error("Your device was reachable but we could not correctly detect it, please file an issue at: https://github.com/dm82m/hass-Deltasol-KM2/issues/new/choose")
+                    #TODO #12 grateful shutdown
+            else:
+                _LOGGER.error("Are you sure you entered the correct address of the Resol Deltasol KM2/DL2/DL3 device? Please re-check and if the issue still persists, please file an issue here: https://github.com/dm82m/hass-Deltasol-KM2/issues/new/choose")
+                #TODO #12 grateful shutdown
                 
-        except Exception as e:
-            _LOGGER.debug(f"Error detecting resol product - {e}")
-
-        _LOGGER.warning("Unable to detect resol system")
+        except RequestException as e:
+            _LOGGER.error(f"Error detecting Resol Deltasol product - {e}, please file an issue at: https://github.com/dm82m/hass-Deltasol-KM2/issues/new/choose")
+            #TODO #12 grateful shutdown
+            
         return self.product
 
 
@@ -75,49 +78,52 @@ class DeltasolApi(object):
 
         response = {}
 
-        if(product=='km2'):
+        if(product == 'km2'):
             response = self.fetch_data_km2()
-        elif(product=='dl2' or product=='dl3'):
+        elif(product == 'dl2' or product == 'dl3'):
             response = self.fetch_data_dlx()
         else:
-            _LOGGER.debug("Invalid mode, unable to retrive data")
+            _LOGGER.error(f"We detected your Resol Deltasol product as {product} and this product is currently not supported. If you want you can file an issue to support this device here: https://github.com/dm82m/hass-Deltasol-KM2/issues/new/choose")
+            #TODO #12 grateful shutdown
 
         return self.__parse_data(response)
 
 
     def fetch_data_km2(self):
-        _LOGGER.debug("Retriving data from km2")
+        _LOGGER.debug("Retrieving data from km2")
+        
         response = {}
+        
         url = f"http://{self.host}/cgi-bin/resol-webservice"
-
+        _LOGGER.debug(f"KM2 requesting sensor data url {url}")
+                
         headers = {
             'Content-Type': 'application/json'
         }
-
         payload = "[{'id': '1','jsonrpc': '2.0','method': 'login','params': {'username': '" + self.username + "','password': '" + self.password + "'}}]"
         response = requests.request("POST", url, headers=headers, data = payload).json()
-
-        _LOGGER.debug("Got valid JSON, assuming that we have a KM2 device ...")
-
+        #TODO #13 handle wrong credentials and grateful shutdown
         authId = response[0]['result']['authId']
         
         payload = "[{'id': '1','jsonrpc': '2.0','method': 'dataGetCurrentData','params': {'authId': '" + authId + "'}}]"
-
         response = requests.request("POST", url, headers=headers, data = payload).json()
-        _LOGGER.debug(f"KM2 response {response}")
+        _LOGGER.debug(f"KM2 response: {response}")
         response = response[0]["result"]
+        
         return response
 
 
     def fetch_data_dlx(self):
         _LOGGER.debug("Retriving data from dlx")
+        
         response = {}
 
         filter = f"filter={self.api_key}&" if self.api_key else ""
         
         url = f"http://{self.host}/dlx/download/live?{filter}sessionAuthUsername={self.username}&sessionAuthPassword={self.password}"
-        _LOGGER.debug(f"DLX requesting sensor data url {url.replace(self.password, '******')}")
+        _LOGGER.debug(f"DLX requesting sensor data url {url.replace(self.password, '***')}")
         
         response = requests.request("GET", url).json()
-        _LOGGER.debug(f"DLX response {response}")
+        #TODO #13 handle wrong credentials and grateful shutdown
+        _LOGGER.debug(f"DLX response: {response}")
         return response
