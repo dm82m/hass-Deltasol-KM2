5318567f3a60ab8d0184c79820cabf137af84c3e
dm82m <dirk@maucher-online.de>
2025-03-07 15:53:19 +0100

    using a custom name for each integration entry to support multiple entries

--- custom_components/deltasol/config_flow.py
+++ custom_components/deltasol/config_flow.py
@@ -59,7 +59,7 @@ async def validate_input(hass: HomeAssistant, data: dict[str, Any]) -> dict[str,
     except IntegrationError as err:
         raise CannotConnect from err
 
-    return {"title": DEFAULT_NAME}
+    return {"title": f"{api.product_details["name"]}@{data.get(CONF_HOST)}:{data.get(CONF_PORT)}"}
 
 
 class ExampleConfigFlow(ConfigFlow, domain=DOMAIN):
